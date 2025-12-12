c2851b0a2cbe3f024cc7e4cd0a602d097963eda2
VitaliiZakharov87 <vitalzs2016@gmail.com>
2025-12-12 21:53:59 +0300

    Update getProductNames.sql

--- getProductNames.sql
+++ getProductNames.sql
@@ -1,4 +1,4 @@
-select o.product_name\
-from CUSTOMERS c\
-    left join ORDERS o on c.id = o.customer_id\
-where c.name = 'alexey';}
+select o.product_name
+from CUSTOMERS c
+    left join ORDERS o on c.id = o.customer_id
+where lower(c.name) = 'alexey';
